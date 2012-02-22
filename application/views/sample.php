a542b6eedcfc313b6049b7eae000c6800c1d7afc
Keiko Tsushima <tusimarimo@gmail.com>
2012-02-22 10:48:08 +0900

    first commit

---
+++ application/views/sample.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>route share</title>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<div class="LyWrap">
+
+<header class="LyHead">
+<h1 class="MdHeadTtl"><a href="#">route share</a></h1>
+<p class="MdHeadLnk"><a href="#">My Page</a></p>
+</header>
+
+<aside>
+<form class="MdSearch"><fieldset>
+<legend>検索</legend>
+<input type="text" class="mdSearchTxt" id="q" name="q" maxlength="255">
+<input type="submit" class="mdSearchBtn" value="検索">
+</fieldset></form>
+</aside>
+
+<nav>
+<ul>
+<li><a href="#">ランキング</a></li>
+<li><a href="#">新着</a></li>
+</ul>
+</nav>
+
+<section>
+<h1>ランキング一覧</h1>
+<ul>
+<li><a href="#">
+<h2>お台場デートコース</h2>
+<p>hogehogehogehogehogehogehogehogehoge</p>
+</a></li>
+</ul>
+</section>
+
+<footer class="LyFoot">
+<p class="MdCopyright"><small>&copy; NHN Japan Corp.</small></p>
+</footer>
+
+</div>
+
+</body>
+</html>
